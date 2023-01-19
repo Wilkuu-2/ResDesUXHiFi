cc29f08a59bb578eb90c468a099db15ae0c2dbeb
EmmavandenBroek <e.m.vandenbroek@student.utwente.nl>
2023-01-19 10:55:21 +0100

    Added dancing flowers in the dance scene

---
+++ Assets/Scripts/DancingFlower/AnimatorController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animator.SetBool("Phase1", true);
+            animator.SetBool("Phase2", false);
+            animator.SetBool("Phase3", false);
+            animator.SetBool("Phase4", false);
+            animator.SetBool("Phase5", false);
+ 
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animator.SetBool("Phase2", true);
+            animator.SetBool("Phase1", false);
+            animator.SetBool("Phase3", false);
+            animator.SetBool("Phase4", false);
+            animator.SetBool("Phase5", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animator.SetBool("Phase3", true);
+            animator.SetBool("Phase2", false);
+            animator.SetBool("Phase1", false);
+            animator.SetBool("Phase4", false);
+            animator.SetBool("Phase5", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            animator.SetBool("Phase4", true);
+            animator.SetBool("Phase2", false);
+            animator.SetBool("Phase3", false);
+            animator.SetBool("Phase1", false);
+            animator.SetBool("Phase5", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            animator.SetBool("Phase5", true);
+            animator.SetBool("Phase2", false);
+            animator.SetBool("Phase3", false);
+            animator.SetBool("Phase4", false);
+            animator.SetBool("Phase1", false);
+        }
+    }
+}
